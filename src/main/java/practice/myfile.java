c509fe62fa44d9a48040cf5899ad8c037ad812e1
Khair <Khair@10.0.0.52>
2023-02-21 12:07:06 -0600

    done

--- src/main/java/practice/myfile.java
+++ src/main/java/practice/myfile.java
@@ -6,8 +6,8 @@ public class myfile {
 
 System.out.println("Welcome for testing ant");
 System.out.println("testing ant 1");
-System.out.println('testing ant 2');
-System.out.println('testing ant 3');
+System.out.println("testing ant ");
+System.out.println("testing ant 3");
 System.out.println("testing new project");
 System.out.println(250-69);
 	}
