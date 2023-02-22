b556412b3cd7d4a65d9a6415d6f475adb2b0f2af
Khair <Khair@10.0.0.52>
2023-02-22 01:21:56 -0600

    maven test

--- src/main/java/practice/myfile.java
+++ src/main/java/practice/myfile.java
@@ -12,7 +12,7 @@ System.out.println("testing new project");
 System.out.println(250-69);
 System.out.println("testing maven1");
 System.out.println("test maven 2");
-
+System.out.println("testing maven with webhooks");
 	}
 
 }
